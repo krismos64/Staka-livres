a062288a6bac62d74a821a14c88db1ce2f7de435
krismos64 <c.mostefaoui@yahoo.fr>
2025-09-04 10:39:40 +0200

    fuseaux horaires mails templates update

--- backend/src/queues/emailQueue.ts
+++ backend/src/queues/emailQueue.ts
@@ -15,7 +15,8 @@ Handlebars.registerHelper('formatDate', function(date: any) {
     month: 'long',
     day: 'numeric',
     hour: '2-digit',
-    minute: '2-digit'
+    minute: '2-digit',
+    timeZone: 'Europe/Paris'
   });
 });
 
@@ -78,7 +79,8 @@ class EmailQueue {
             month: 'long',
             day: 'numeric',
             hour: '2-digit',
-            minute: '2-digit'
+            minute: '2-digit',
+            timeZone: 'Europe/Paris'
           }) : ''
       };
       
